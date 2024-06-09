eba793a3cbecb35b214dbff5524aed384f618ccb
Axel Grande Ruiz <shadow1234f@outlook.com>
2024-06-09 14:17:08 -0600

    threads

--- highlighter/highlighter.py
+++ highlighter/highlighter.py
@@ -1,12 +1,72 @@
+STYLE = """"
+.type {
+    color: #0000FF;
+    display: inline;
+}
 
+body {
+    background: #1c1c1c;
+    font-family: 'Courier New', Courier, monospaces;
+}
+
+/* p {
+    display: inline;
+} */
+
+.reserved-word{
+    color: #4A9CD6;
+    display: inline;
+}
+
+.comment {
+    color: #84B371;
+    display: inline;
+}
+
+.operator {
+    color: #fff;
+    display: inline;
+}
+
+.literal{
+    color: #a9cbea;
+    display: inline;
+}
+
+.string{
+    color: rgb(209, 132, 31);
+    display: inline;
+}
+
+.function{
+    color: #DCDCAA;
+    display: inline;
+}
+
+.delimiter{
+    color: #EDDA61;
+    display: inline;
+}
+
+.variable {
+    color: rgb(82, 189, 199);
+    display: inline;
+}
+
+.control{
+    color: #ea7ae6;
+    display: inline;
+}
+"""
 class Highlighter:
     
-    def __init__(self):
-        self.file = open("output.html", "w")
+    def __init__(self, output_path):
+        self.file = open(output_path, "w")
     
     def start_doc(self):
         self.file.write("<html>\n")
-        self.file.write("<head>\n   <link rel='stylesheet' type='text/css' href='highlighter/styles.css'>\n<head>\n\n")
+        self.file.write("<head>\n   <link rel='stylesheet' type='text/css' href='styles.css'>\n<head>\n\n")
+        self.file.write(f"<style>\n{STYLE}\n<style>\n\n")
         self.file.write("\n<body>\n")
 
     def end_doc(self):
--- main.py
+++ main.py
@@ -1,21 +1,45 @@
 from highlighter.highlighter import Highlighter
 from token_separator.token_separator import TokenSeparator
 import argparse
+import threading
+import time
+import os
 
-def parse_args():
-    '''parses the command line arguments'''
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('--path', type=str, default="tests/test.txt", help="path to test file")
+def get_all_files(directory):
+    files = []
+    for root, _, filenames in os.walk(directory):
+        for filename in filenames:
+            files.append(os.path.join(root, filename))
+    return files
 
-    return parser.parse_args()
+def get_ext(file_path):
+    ext = os.path.splitext(file_path)[-1]
+    if ext[0] == ".":
+        return ext[1:]
+    return ext
 
+def remove_invalid_exts(file_paths, valid):
+    ret = []
 
-def main():
-    args = parse_args()
-    token = TokenSeparator(types_path="types.txt", process_file=args.path).run()
+    for file in file_paths:
+        file_ext = get_ext(file)
 
-    # Highlight in html
-    highlight = Highlighter()
+        if file_ext in valid:
+            ret.append(file)
+
+    return ret
+
+def remove_extension(file_path):
+    return os.path.splitext(file_path)[0]
+
+def highlighter(path):
+    
+    token = TokenSeparator(types_path="types.txt", process_file=path).run()
+
+    # Path without extension
+    html_output = remove_extension(path) + get_ext(path) + ".html"
+
+    highlight = Highlighter(html_output)
     highlight.start_doc()
 
     counter = 0
@@ -36,8 +60,26 @@ def main():
     highlight.end_doc()
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    start_time = time.time()
+    valid = [".cpp", ".py", ".txt"]
+    process_files = get_all_files('./tests')
 
-    
+    process_files = remove_invalid_exts(process_files, valid)
+
+    threads = []
+
+    for file in process_files:
+        # print(file)
+        # continue
+        t1 = threading.Thread(target=highlighter, args=(file,))
+        t1.start()
+        threads.append(t1)
+
+    for thread in threads:
+        thread.join()
+
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"Execution time: {execution_time} seconds")
 
